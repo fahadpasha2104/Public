b0451f7ef0495ce5760280299b64c40c6f0efd04
hackerfantastic@googlemail.com <hackerfantastic@googlemail.com>
2016-07-07 14:08:46 +0100

    BBC:microbit game of life

--- misc/microbit_life.py
+++ misc/microbit_life.py
@@ -11,11 +11,11 @@ def game_of_life():
 	for row in range(cellcount[0]):
 		for col in range(cellcount[1]):
 			state["%d,%d" % (row,col)] = random.randint(0,1)
-	## set a state
+	## set a state 
 #	state = {
-#		"0,0":1, "0,1":1,"0,2":0, "0,3": 0, "0,4": 0,
-#		"1,0":1, "1,1":0,"1,2":1, "1,3": 0, "1,4": 0,
-#		"2,0":1, "2,1":0,"2,2":0, "2,3": 0, "2,4": 0,
+#		"0,0":0, "0,1":0,"0,2":1, "0,3": 0, "0,4": 0,
+#		"1,0":0, "1,1":0,"1,2":0, "1,3": 1, "1,4": 0,
+#		"2,0":0, "2,1":1,"2,2":1, "2,3": 1, "2,4": 0,
 #		"3,0":0, "3,1":0,"3,2":0, "3,3": 0, "3,4": 0,
 #		"4,0":0, "4,1":0,"4,2":0, "4,3": 0, "4,4": 0,
 #		}
@@ -24,7 +24,7 @@ def game_of_life():
 			for col in range(cellcount[1]):
 				mystate = state["%d,%d" % (row,col)]
 				if mystate == 1:
-				    microbit.display.set_pixel(row,col,9)
+					microbit.display.set_pixel(row,col,9)
 				else:
 					microbit.display.set_pixel(row,col,0)
 		for row in range(cellcount[0]):
