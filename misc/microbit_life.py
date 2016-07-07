7a12c32b2fa51873e9486807bdab18abe8e340a4
hackerfantastic@googlemail.com <hackerfantastic@googlemail.com>
2016-07-07 14:18:08 +0100

    BBC:microbit game of life

--- misc/microbit_life.py
+++ misc/microbit_life.py
@@ -12,13 +12,13 @@ def game_of_life():
 		for col in range(cellcount[1]):
 			state["%d,%d" % (row,col)] = random.randint(0,1)
 	## set a state 
-#	state = {
-#		"0,0":0, "0,1":0,"0,2":1, "0,3": 0, "0,4": 0,
-#		"1,0":0, "1,1":0,"1,2":0, "1,3": 1, "1,4": 0,
-#		"2,0":0, "2,1":1,"2,2":1, "2,3": 1, "2,4": 0,
-#		"3,0":0, "3,1":0,"3,2":0, "3,3": 0, "3,4": 0,
-#		"4,0":0, "4,1":0,"4,2":0, "4,3": 0, "4,4": 0,
-#		}
+	#state = {
+	#	"0,0":0, "0,1":0,"0,2":0, "0,3": 0, "0,4": 0,
+	#	"1,0":0, "1,1":0,"1,2":1, "1,3": 0, "1,4": 0,
+	#	"2,0":0, "2,1":0,"2,2":1, "2,3": 0, "2,4": 1,
+	#	"3,0":0, "3,1":0,"3,2":1, "3,3": 1, "3,4": 0,
+	#	"4,0":0, "4,1":0,"4,2":0, "4,3": 0, "4,4": 0,
+	#	}
 	for generation in range(genrange):
 		for row in range(cellcount[0]):
 			for col in range(cellcount[1]):
@@ -27,6 +27,7 @@ def game_of_life():
 					microbit.display.set_pixel(row,col,9)
 				else:
 					microbit.display.set_pixel(row,col,0)
+					
 		for row in range(cellcount[0]):
 			for col in range(cellcount[1]):
 				moore = 0
